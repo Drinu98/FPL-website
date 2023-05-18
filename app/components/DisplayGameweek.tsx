00364b7528fb90f3f5e71f3764854cb91e82625b
Andre Galea <andregaleaweb@gmail.com>
2023-05-18 13:49:35 +0200

    gameweek changes

---
+++ app/components/DisplayGameweek.tsx
@@ -0,0 +1,41 @@
+"use client"
+// type Gameweek = {
+//     id: number
+//     deadline_time: string
+// }
+
+type DisplayGameweekProps = {
+    id: number
+    deadline_time: string
+}
+
+const DisplayGameweek = (props: DisplayGameweekProps) => {
+    const {id, deadline_time} = props
+
+    const deadlineTime = new Date(deadline_time);
+    const deadlineDate = deadlineTime.toLocaleString('en-GB', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+        weekday: 'short',
+    });
+    const startTime = deadlineTime.toLocaleString('en-GB', {
+        hour: 'numeric',
+        minute: 'numeric',
+        hourCycle: 'h12',
+    });
+
+    return (
+        <>
+            <div className='graphic-container-deadline'>
+                <h2 className='transfers-title'>Deadline</h2>            
+            </div>
+            <div>
+                <h2 className="deadline-date">Gameweek {id}</h2>
+                <p className="deadline-date-time">{deadlineDate} {startTime}</p>
+            </div>
+        </>
+    );
+}
+
+export default DisplayGameweek
--- app/components/gameweek.js
+++ app/components/gameweek.js
@@ -1,4 +1,4 @@
-
+import DisplayGameweek from "./DisplayGameweek";
 
 async function getNextGameweek() {
     const res = await fetch('https://fantasy.premierleague.com/api/bootstrap-static/', 
@@ -23,33 +23,36 @@ async function getNextGameweek() {
 
 export default async function GameweekInfo() {
 
-    const data = await getNextGameweek();
+    const gameweeks = await getNextGameweek();
 
-    const currentGameweek = data.find(gw => !gw.is_current && gw.is_next);
+    const currentGameweek = gameweeks.find(gw => !gw.is_current && gw.is_next);
 
-    const deadlineTime = new Date(currentGameweek.deadline_time);
-    const deadlineDate = deadlineTime.toLocaleString('en-GB', {
-        day: 'numeric',
-        month: 'long',
-        year: 'numeric',
-        weekday: 'short',
-    });
-    const startTime = deadlineTime.toLocaleString('en-GB', {
-        hour: 'numeric',
-        minute: 'numeric',
-        hourCycle: 'h12',
-    });
+    // const nextGameweek = [currentGameweek.id, currentGameweek.deadline_time];
+
+    // const deadlineTime = new Date(currentGameweek.deadline_time);
+    // const deadlineDate = deadlineTime.toLocaleString('en-GB', {
+    //     day: 'numeric',
+    //     month: 'long',
+    //     year: 'numeric',
+    //     weekday: 'short',
+    // });
+    // const startTime = deadlineTime.toLocaleString('en-GB', {
+    //     hour: 'numeric',
+    //     minute: 'numeric',
+    //     hourCycle: 'h12',
+    // });
 
     return (
-        <>
-            <div className='graphic-container-deadline'>
-                <h2 className='transfers-title'>Deadline</h2>            
-            </div>
-            <div>
-                <h2 className="deadline-date">Gameweek {currentGameweek.id}</h2>
-                <p className="deadline-date-time">{deadlineDate} {startTime}</p>
-            </div>
-        </>
+        // <>
+        //     <div className='graphic-container-deadline'>
+        //         <h2 className='transfers-title'>Deadline</h2>            
+        //     </div>
+        //     <div>
+        //         <h2 className="deadline-date">Gameweek {currentGameweek.id}</h2>
+        //         <p className="deadline-date-time">{deadlineDate} {startTime}</p>
+        //     </div>
+        // </>
+        <DisplayGameweek id={currentGameweek.id} deadline_time={currentGameweek.deadline_time} />
     );
 
 }
