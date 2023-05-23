2289d7afa9dde001a14a43101032c060b60d1ccf
Andre Galea <andregaleaweb@gmail.com>
2023-05-23 09:56:05 +0200

    pricechanges change

--- app/api/pricechanges/route.ts
+++ app/api/pricechanges/route.ts
@@ -4,7 +4,7 @@ import { prisma } from "../../../services/prisma";
 let risingPlayers = [] as Array<any>
 let fallingPlayers = [] as Array<any>
 
-export async function GET(){
+export async function handler(){
   console.log("Gathering Price changes");
   const risers = new Map();
   const fallers = new Map();  
