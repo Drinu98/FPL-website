fa4719169dd41375e9250c297205a7575576d02f
Andre Galea <andregaleaweb@gmail.com>
2024-01-12 17:53:19 +0100

    config changes.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // staticPageGenerationTimeout: 2000,
+  staticPageGenerationTimeout: 200,
   images: {
     domains: ['resources.premierleague.com'], // Add 'resources.premierleague.com' to the domains array
   },
